b96f8c65107c80ad2652ad0dadac2ab8fe243504
Andrew <appoteres@crimson.ua.edu>
2019-03-06 11:41:35 -0600

    Beginning of the board generator

---
+++ boardgen.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct
+{
+	int length ;
+	int height ;
+
+} board;
+
+
+board *newBoard(int l, int h){
+	for (int i = 0;int i<=5;i++){
+		printf("#");
+	}
+
+
+
+}
+
+#ifndef TESTINGBOARD
+int main(){
+board this = newBoard(5,5);
+
+}
+
+#endif
